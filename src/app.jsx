82912ac6440e4f16044563691378e96ad7af58f0
developer <coderdarkllc@gmail.com>
2024-10-20 19:51:40 -0500

    added vite and tailwindcss

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+    return <main>
+        <header className="text-center text-3xl text-white">
+            coderdark
+        </header>
+        <section className="text-center text-white">
+
+        </section>
+        <footer className="text-center text-white">
+            © Copyright 2024 coderdark
+        </footer>
+    </main>;
+}
